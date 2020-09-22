d222b674000296b744023d0dc89deb5ac1533d02
Gabriel Tiste <gabriel.tiste@seb.se>
2020-09-22 21:40:43 +0200

    boilerplate for mq

---
+++ XB.Astrea.Connector/LocalMqReader.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.Extensions.Logging;
+using PEX.Connectors.MQ.Reader;
+using PEX.Connectors.MQAdapter;
+using PMC.Common.Wrappers;
+
+namespace XB.Astrea.Connector
+{
+    public class LocalMqReader : MqReader
+    {
+        private readonly ILogger<LocalMqReader> _logger;
+        public LocalMqReader(IMqQueueProcessor mqQueueProcessor, IConfigurationManagerWrapper configurationManagerWrapper, ILogger<LocalMqReader> logger) : base(mqQueueProcessor, configurationManagerWrapper)
+        {
+            _logger = logger;
+        }
+
+        public override void ProcessMessage(MqMessage message)
+        {
+            _logger.LogInformation(message.Data);
+        }
+
+
+    }
+}
--- XB.Astrea.Connector/Program.cs
+++ XB.Astrea.Connector/Program.cs
@@ -1,9 +1,14 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using PEX.Connectors.MQ.Reader;
+using PEX.Connectors.MQAdapter;
+using PMC.Common.Logging;
+using PMC.Common.Threading;
+using PMC.Common.Wrappers;
+using XB.Astrea.Connector.Logger;
 
 namespace XB.Astrea.Connector
 {
@@ -18,6 +23,22 @@ namespace XB.Astrea.Connector
             Host.CreateDefaultBuilder(args)
                 .ConfigureServices((hostContext, services) =>
                 {
+                    services.AddTransient<IMqAdapterFactory, MqAdapterFactory>();
+                    services.AddTransient<IMqQueuePoller, MqQueuePoller>();
+                    services.AddTransient<IClientLogger, ClientLogger>();
+                    services.AddTransient<IThreadExceptionHandler, ThreadExceptionHandler>();
+                    services.AddTransient<IThreadSleep, ThreadSleepWrapper>();
+                    services.AddTransient<IActionExceptionHandler, ActionExceptionHandler>();
+                    services.AddTransient<ITaskFactory, TaskFactory>();
+                    services.AddTransient<IMqMessageBuilder, MqMessageBuilder>();
+
+                    services.AddTransient<IMqQueueProcessor, MqQueueProcessor>();
+                    services.AddTransient<IConfigurationManagerWrapper, ConfigurationManagerWrapper>();
+
+                    services.AddSingleton<IMqQueueManagerFactory, MqQueueManagerFactory>();
+
+                    services.AddTransient<IMqReader, LocalMqReader>();
+
                     services.AddHostedService<Worker>();
                 });
     }
