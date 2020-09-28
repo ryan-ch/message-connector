5bdd78e4eacb2dcaec0c00b35583fc8777d17075
Gabriel Tiste <gabriel.tiste@seb.se>
2020-09-28 12:39:58 +0200

    recreated dockerfile

---
+++ MqTool/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Threading;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using XB.IBM.MQ;
+
+namespace MqTool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var startup = new Startup();
+            var mqClient = startup.Provider.GetService<IMqClient>();
+            mqClient.Start();
+
+            bool continueProgram = true;
+
+            while (continueProgram)
+            {
+                Console.WriteLine("What do you want to do?");
+                Console.WriteLine("1. Write messages?");
+                Console.WriteLine("0. Quit");
+                Console.Write(": ");
+
+                string input = Console.ReadLine();
+
+                switch (input)
+                {
+                    case "1":
+                        Console.Write("How many messages?: ");
+                        int iterations = Int32.Parse((Console.ReadLine()));
+                        for (int i = 0; i < iterations; i++)
+                        {
+                            mqClient.WriteMessageAsync("test " + i, new CancellationToken());
+                        }
+                        break;
+                    case "0":
+                        continueProgram = false;
+                        break;
+
+                }
+            }
+
+            mqClient.Stop();
+        }
+    }
+}
---
+++ MqTool/Startup.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using XB.IBM.MQ;
+
+namespace MqTool
+{
+    public class Startup
+    {
+        private readonly IConfiguration _configuration;
+        private readonly IServiceProvider _provider;
+
+        public Startup()
+        {
+            _configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile($"appsettings.Development.json", optional: true)
+                .Build();
+
+            // instantiate
+            var services = new ServiceCollection();
+
+            // add necessary services
+            services.AddLogging(configure => configure.AddConsole());
+            services.AddSingleton<IConfiguration>(_configuration);
+            services.AddTransient<IMqClient, MqClient>();
+
+            // build the pipeline
+            _provider = services.BuildServiceProvider();
+        }
+
+        // access the built service pipeline
+        public IServiceProvider Provider => _provider;
+
+        // access the built _configuration
+        public IConfiguration Configuration => _configuration;
+    }
+}
