a6a61d58a83ddd9c7aff87c4d848af925b35bb41
Sandra Strzelczak <sandra.strzelczak@seb.se>
2021-02-10 15:30:18 +0100

    Added seb cs nuget package to XB Hubert project

--- Dependencies/XB.Hubert/HubertClient.cs
+++ Dependencies/XB.Hubert/HubertClient.cs
@@ -1,4 +1,8 @@
-﻿namespace XB.Hubert
+﻿using System;
+using System.Net.Http;
+using SEB.SEBCS.RTM.v1.Client.Uakm463.Crossbordpmt.Update01.Fcpsts01;
+
+namespace XB.Hubert
 {
     public class HubertClient : IHubertClient
     {
