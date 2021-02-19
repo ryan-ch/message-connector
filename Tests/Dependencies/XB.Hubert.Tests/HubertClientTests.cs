667c9898bf21c5ac8b636c329d4e50153ff0c7b8
Sandra Strzelczak <sandra.strzelczak@seb.se>
2021-02-19 14:47:14 +0100

    setup for unit tests works

--- Tests/Dependencies/XB.Hubert.Tests/HubertClientTests.cs
+++ Tests/Dependencies/XB.Hubert.Tests/HubertClientTests.cs
@@ -11,24 +11,37 @@ namespace XB.Hubert.Tests
     public class HubertClientTests
     {
         private readonly Mock<IOptions<HubertClientOptions>> _configMock;
-        //private readonly HttpClient _httpClient;
+        private readonly Mock<HttpClient> _httpClientMock;
         private readonly Mock<ILogger<HubertClient>> _loggerMock;
         private readonly Mock<IHttpClientFactory> _clientFactoryMock;
         private readonly IHubertClient _hubertClient;
+        private readonly HubertClientOptions _hubertClientOptions;
 
         public HubertClientTests()
         {
             _configMock = new Mock<IOptions<HubertClientOptions>>();
+            _httpClientMock = new Mock<HttpClient>();
             _loggerMock = new Mock<ILogger<HubertClient>>();
             _clientFactoryMock = new Mock<IHttpClientFactory>();
+            _hubertClientOptions = new HubertClientOptions()
+            {
+                Url = "https://sfsfsdfdsfsdfdsdfssdf.se"
+            };
 
-            //_hubertClient = new HubertClient(_clientFactoryMock.Object, _configMock.Object, _loggerMock.Object);
+            _clientFactoryMock.Setup(a => a.CreateClient(It.IsAny<string>())).Returns(_httpClientMock.Object);
+            _configMock.Setup(a => a.Value).Returns(_hubertClientOptions);
+
+            _hubertClient = new HubertClient(_clientFactoryMock.Object, _configMock.Object, _loggerMock.Object);
         }
 
         [Fact]
         public void SendAssessmentResponse_()
         {
+            //Arrange
 
+            //Act
+
+            //Assert
         }
     }
 }
