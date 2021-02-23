5aa1885074d2a6df92185396f40ae8dad38887df
Sandra Strzelczak <sandra.strzelczak@seb.se>
2021-02-23 17:01:19 +0100

    updated hubert unit test

--- Tests/Dependencies/XB.Hubert.Tests/HubertClientTests.cs
+++ Tests/Dependencies/XB.Hubert.Tests/HubertClientTests.cs
@@ -1,8 +1,14 @@
 using System;
 using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Moq;
+using Moq.Protected;
+using Newtonsoft.Json;
+using SEB.SEBCS.RTM.v1.Client.Uakm463;
+using SEB.SEBCS.RTM.v1.Client.Uakm463.Crossbordpmt.Update01.Fcpsts01;
 using XB.Hubert.Config;
 using Xunit;
 
@@ -28,20 +34,50 @@ namespace XB.Hubert.Tests
                 Url = "https://sfsfsdfdsfsdfdsdfssdf.se"
             };
 
-            _clientFactoryMock.Setup(a => a.CreateClient(It.IsAny<string>())).Returns(_httpClientMock.Object);
-            _configMock.Setup(a => a.Value).Returns(_hubertClientOptions);
+            var mockHttpMessageHandler = new Mock<HttpMessageHandler>();
+            mockHttpMessageHandler.Protected()
+                .Setup<Task<HttpResponseMessage>>(
+                    "SendAsync",
+                    ItExpr.IsAny<HttpRequestMessage>(),
+                    ItExpr.IsAny<CancellationToken>()
+                )
+                .ReturnsAsync((HttpRequestMessage request, CancellationToken token) =>
+                {
+                    HttpResponseMessage response = new HttpResponseMessage();
+                    response.Content = new StringContent(JsonConvert.SerializeObject(new CrossbordpmtUpdate01Fcpsts01Response()
+                    {
+                        Result = new CrossbordpmtUpdate01Fcpsts01Tables()
+                        {
+                            Uakw4630 = new UAKW4630()
+                            {
+                                CreateTimestamp = "2020-02-12 11:38:00.000000",
+                                Guid = "ced0f305-f722-4855-bfc7-5da3bf38bebc",
+                                RowId = 1,
+                                SourceId =  "TEST",
+                                TransactionStatus = "REJECTED"
+                            }
+                        }
+                    }));
+                    return response;
+                });
 
+            var httpClient = new HttpClient(mockHttpMessageHandler.Object);
+
+            _clientFactoryMock.Setup(a => a.CreateClient(It.IsAny<string>())).Returns(httpClient);
+            _configMock.Setup(a => a.Value).Returns(_hubertClientOptions);
             _hubertClient = new HubertClient(_clientFactoryMock.Object, _configMock.Object, _loggerMock.Object);
         }
 
         [Fact]
-        public void SendAssessmentResponse_()
+        public void SendAssessmentResponse_WillCreateAndPostRequest()
         {
-            //Arrange
+            const string timestamp = "";
+            const string guid = "ced0f305-f722-4855-bfc7-5da3bf38bebc";
+            const string transactionStatus = "";
 
-            //Act
+            var response = _hubertClient.SendAssessmentResponse(timestamp, guid, transactionStatus).Result;
 
-            //Assert
+            Assert.Equal(response.Result.Uakw4630.Guid, guid);
         }
     }
 }
