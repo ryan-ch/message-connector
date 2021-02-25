767244b4a69c7fc2fc1fbe43061436bf0668c35f
Gabriel Tiste <gabriel.tiste@seb.se>
2021-02-25 12:42:39 +0100

    reverted to early exit

--- Dependencies/XB.HttpClient.Jwt/AuthenticationDelegatingHandler.cs
+++ Dependencies/XB.HttpClient.Jwt/AuthenticationDelegatingHandler.cs
@@ -52,42 +52,34 @@ namespace XB.HttpClientJwt
 
         private async Task<string> GetJwt(CancellationToken cancellationToken)
         {
-            if (string.IsNullOrEmpty(_jwt) || DateTimeOffset.UtcNow.ToUnixTimeSeconds() > _jwtExpire)
+            if (!string.IsNullOrEmpty(_jwt) || DateTimeOffset.UtcNow.ToUnixTimeSeconds() < _jwtExpire)
             {
-                var jwtRequest = new HttpRequestMessage(HttpMethod.Post, new Uri(_httpClientJwtOptions.Url));
-
-                var jwtDetails = new Dictionary<string, string>
-                {
-                    { "grant_type", _httpClientJwtOptions.Grant_Type },
-                    { "client_id", _httpClientJwtOptions.ClientId },
-                    { "client_secret", _httpClientJwtOptions.ClientSecret },
-                    { "username", _httpClientJwtOptions.Username },
-                    { "password", _httpClientJwtOptions.Password },
-                    { "scope", _httpClientJwtOptions.Scope },
-                };
-
-                jwtRequest.Content = new FormUrlEncodedContent(jwtDetails);
-                
-                var currentTime = DateTimeOffset.UtcNow.ToUnixTimeSeconds();
-
-                var response = await base.SendAsync(jwtRequest, cancellationToken);
-
-                var content = JObject.Parse(await response.Content.ReadAsStringAsync(cancellationToken));
-
-                _jwt = content?.SelectToken("access_token")?.ToString();
-                _jwtExpire = long.Parse(content?.SelectToken("expires_in")?.ToString() ?? "0", CultureInfo.InvariantCulture)
-                             + currentTime;
-
-                //TODO: Maybe we can remove this logging?
-                _logger.LogInformation(
-                    $"Successfully fetched an new JWT for user {_httpClientJwtOptions.ClientId}.");
-
                 return _jwt;
             }
 
-            //TODO: Maybe we can remove this logging?
-            _logger.LogInformation(
-                $"Successfully re-used JWT for user {_httpClientJwtOptions.ClientId}.");
+            var jwtRequest = new HttpRequestMessage(HttpMethod.Post, new Uri(_httpClientJwtOptions.Url));
+
+            var jwtDetails = new Dictionary<string, string>
+            {
+                { "grant_type", _httpClientJwtOptions.Grant_Type },
+                { "client_id", _httpClientJwtOptions.ClientId },
+                { "client_secret", _httpClientJwtOptions.ClientSecret },
+                { "username", _httpClientJwtOptions.Username },
+                { "password", _httpClientJwtOptions.Password },
+                { "scope", _httpClientJwtOptions.Scope },
+            };
+
+            jwtRequest.Content = new FormUrlEncodedContent(jwtDetails);
+
+            var currentTime = DateTimeOffset.UtcNow.ToUnixTimeSeconds();
+
+            var response = await base.SendAsync(jwtRequest, cancellationToken);
+
+            var content = JObject.Parse(await response.Content.ReadAsStringAsync(cancellationToken));
+
+            _jwt = content?.SelectToken("access_token")?.ToString();
+            _jwtExpire = long.Parse(content?.SelectToken("expires_in")?.ToString() ?? "0", CultureInfo.InvariantCulture)
+                         + currentTime;
 
             return _jwt;
         }
