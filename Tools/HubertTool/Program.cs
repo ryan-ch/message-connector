de023052d1f2dfa6d7b5cc058718c7bb5923ba51
Karolin Granvald <karolin.granvald@seb.se>
2021-03-04 14:52:03 +0100

    added hubert-tool

---
+++ Tools/HubertTool/Program.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+using XB.Hubert;
+
+namespace HubertTool
+{
+    class Program
+    {
+        private static async Task Main(string[] args)
+        {
+            var startup = new Startup();
+            var hubertClient = startup.Provider.GetRequiredService<IHubertClient>();
+
+            Random rnd = new Random();
+            
+            while (true)
+            {
+                Console.WriteLine("Enter amount of assessments to send to Hubert: ");
+                var amount = int.Parse(Console.ReadLine()!);
+
+                var sTime = DateTime.Now;
+                Console.WriteLine(sTime.ToString("yyyy-MM-ddTHH:mm:ss.fff"));
+                for (int i = 0; i < amount; i++)
+                {
+                    _ = Task.Run(async () =>
+                    {
+                        var result = await hubertClient.SendAssessmentResultAsync(DateTime.Now.ToString(),
+                            Guid.NewGuid().ToString(), rnd.Next(0, 10).ToString());
+                        if (result.Result != null)
+                        {
+                            Console.WriteLine(DateTime.Now.ToString("yyyy-MM-ddTHH:mm:ss.fff") + " ===== Status from Hubert: " + result.Result.Uakw4630.TransactionStatus + " Thread: " + Thread.CurrentThread.ManagedThreadId);
+                        }
+                    });
+                }
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Tools/HubertTool/Startup.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using XB.HttpClientJwt;
+using XB.Hubert;
+
+namespace HubertTool
+{
+    
+    public class Startup
+    {
+        private readonly IConfiguration _configuration;
+        public IServiceProvider Provider { get; }
+        public Startup()
+        {
+            _configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile($"appsettings.Development.json", optional: true)
+                .Build();
+
+            var services = new ServiceCollection();
+
+            // add necessary services
+            //services.AddLogging(configure => configure.AddConsole());
+            services.AddSingleton(_configuration);
+            services.AddHttpClientJwt(_configuration, "AppSettings:");
+            services.AddHubert(_configuration, "AppSettings:");
+            
+
+            // build the pipeline
+            Provider = services.BuildServiceProvider();
+
+        }
+    }
+}
