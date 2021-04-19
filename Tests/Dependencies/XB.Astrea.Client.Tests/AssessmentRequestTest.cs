10ee2bd84409d8ca5ca360a72f1150a4113dcb09
MF <mosab.falyoun@seb.se>
2021-04-19 15:31:16 +0800

    Enhance the unit tests

--- Tests/Dependencies/XB.Astrea.Client.Tests/AssessmentRequestTest.cs
+++ Tests/Dependencies/XB.Astrea.Client.Tests/AssessmentRequestTest.cs
@@ -11,11 +11,12 @@ namespace XB.Astrea.Client.Tests
     {
 
         [Fact]
-        public void Parse_Mt103ToAstreaRequest_ShouldParseRequestWithCorrectValues()
+        public void AssessmentRequest_WhenInitiatedWithMt103Message_ShouldParseRequestWithCorrectValues()
         {
-            var mt103SwiftMessage = new Mt103Message(SwiftMessagesMock.SwiftMessage_2.OriginalMessage, null);
-            var request = new AssessmentRequest(mt103SwiftMessage);
+            // Act
+            var request = new AssessmentRequest(Mt103MessagesMock.Mt103Message_02);
 
+            // Assert
             Assert.True(Guid.TryParse(request.OrderIdentity, out var newGuid));
             Assert.Equal("E01EBC0C-0B22-322A-A8F1-097839E991F4", request.BasketIdentity);
             Assert.Equal(SwiftMessagesMock.SwiftMessage_2.OriginalMessage, request.Mt);
@@ -36,6 +37,38 @@ namespace XB.Astrea.Client.Tests
 
             Assert.Equal("50601001079", paymentInstruction.CreditAccount.First().Identity);
             Assert.Equal("bban", paymentInstruction.CreditAccount.First().Type);
+        }
+
+        [Fact]
+        public void AssessmentRequest_WhenDebitAccountIsEmpty_ShouldUsePartyIdentifier()
+        {
+            // Arrange
+            var swiftMessage = SwiftMessagesMock.SwiftMessage_1.OriginalMessage.Replace(":50F:/SE2880000832790000012345", ":50F:DRLC/BE/BRUSSELS/NB09490442");
+            var mt103SwiftMessage = new Mt103Message(swiftMessage, null);
+
+            // Act
+            var request = new AssessmentRequest(mt103SwiftMessage);
+
+            // Assert
+            var paymentInstruction = request.PaymentInstructions[0];
+            Assert.Equal("DRLC/BE/BRUSSELS/NB09490442", paymentInstruction.DebitAccount.First().Identity);
+            Assert.Equal("iban", paymentInstruction.DebitAccount.First().Type);
+        }
+
+        [Fact]
+        public void AssessmentRequest_WhenCreditAccountIsEmpty_ShouldUseEmptyArray()
+        {
+            // Arrange
+            var swiftMessage = SwiftMessagesMock.SwiftMessage_1.OriginalMessage.Replace(":59F:/SE3550000000054910123123\r\n1/BOB BAKER", ":59F:1/BOB BAKER");
+            var mt103SwiftMessage = new Mt103Message(swiftMessage, null);
+
+            // Act
+            var request = new AssessmentRequest(mt103SwiftMessage);
+
+            // Assert
+            var paymentInstruction = request.PaymentInstructions[0];
+            Assert.NotNull(paymentInstruction.CreditAccount);
+            Assert.Empty(paymentInstruction.CreditAccount);
         }
     }
 }
